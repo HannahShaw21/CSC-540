fdcea059674c244a7301d6b58c811202d7b3950b
rithviksri <143544332+rithviksri@users.noreply.github.com>
2026-04-10 12:37:31 -0400

    Update GutenbergConnection.java
    added DeletePublication in here

--- GutenbergConnection.java
+++ GutenbergConnection.java
@@ -27,6 +27,7 @@ public class GutenbergConnection {
 
     public static final Class[] OPERATIONS = new Class[]{
             UpdatePublication.class
+            DeletePublication.class
     };
 
     /**
